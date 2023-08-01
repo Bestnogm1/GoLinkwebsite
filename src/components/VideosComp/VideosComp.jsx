cc4e3df6e63e6c81bf2b9a744f2acf597adbf5a9
Bestnogm1 <koffiarielhessou@gmail.com>
2023-07-31 20:38:49 -0400

    new web

--- src/components/VideosComp/VideosComp.jsx
+++ src/components/VideosComp/VideosComp.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import shopMonkeyVideo from "../../video/golink.mp4";
+import ValastroVideo from "../../video/Valastro_V3.mp4";
 import ReactPlayer from "react-player";
 import "./VideosCss.css";
 // import poster from "../../image/golinkThumbNail.jpg";
-import poster from "../../image/golinkThumbNail.png";
+import poster from "../../image/valstro.png";
 // src/image/Image 12-2-22 at 2.19 PM.jpg
 function VideosComp(props) {
   return (
     <div>
       <>
         <video controls class="video" poster={poster}>
-          <source src={shopMonkeyVideo} type="video/mp4" />
+          <source src={ValastroVideo} type="video/mp4" />
         </video>
       </>
     </div>
